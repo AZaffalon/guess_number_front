21ff917ee59c6f7d48c95ef04ed288a543e2b6f5
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2023-09-13 12:14:01 +0200

    Some refactoring and basic front for Teams component

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -29,24 +29,62 @@ export const isTokenValid = (token) => {
     }
 }
 
+/**
+ * Find corresponding user nickname
+ * @param rank_uuid_string
+ * @returns {Promise<*>}
+ */
+export const getUserNickname = (users, rank_uuid_string) => {
+    const split_rank_uuid = rank_uuid_string.split('/')
+    const rank_uuid = split_rank_uuid[split_rank_uuid.length -1]
+
+    const user = users.find(user => user.uuid === rank_uuid )
+
+    return user && (user.nickname !== undefined) ? user.nickname : "Inconnu"
+}
+
 
 export default function Root() {
 
     const [currentToken, setCurrentToken] = useState(null)
+    const [users, setUsers] = useState([])
+
+
+    /**
+     * Call api to save all users in useState
+     * @returns {Promise<void>}
+     */
+    async function getAllUsers(token) {
+        const response = await fetch(`${import.meta.env.VITE_HOST_URL}/api/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            }
+        })
+        if(response.ok) {
+            const data = await response.json()
+            setUsers(data)
+        } else (
+            console.error("Error fetching all users")
+        )
+    }
 
     // Get authentication token
-    useEffect(() => {
+    useEffect( () => {
         const storedToken = localStorage.getItem('authToken')
 
         if(storedToken) {
             setCurrentToken(storedToken)
+            getAllUsers(storedToken)
         }
     }, []);
 
     return (
         <CurrentTokenContext.Provider value={{
             currentToken,
-            setCurrentToken
+            setCurrentToken,
+            users
         }}>
             <Navbar />
                 <Outlet />
